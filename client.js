c507db1b10a61bb711b3eb42e23ced296b771657
Arthur Udalov <artur.udalov@gmail.com>
2015-10-29 00:09:55 +0300

    Separate AJAX and view code

--- client.js
+++ client.js
@@ -1,22 +1,25 @@
 document.addEventListener('DOMContentLoaded',() => {
 
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/messages', false);
-    xhr.send();
-    if (xhr.status != 200) {
-        alert('Не вышло :(');
-    } else {
-        var messages = JSON.parse(xhr.responseText),
-            li_items = new DocumentFragment();
-
-        messages.forEach((message) => {
-            var li = document.createElement('li');
-            li.innerText = message.text;
-            li_items.appendChild(li);
-        });
-
-        document.getElementById('messages').appendChild(li_items);
+    function getMessages() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/messages', false);
+        xhr.send();
+        if (xhr.status != 200) {
+            alert('Не вышло :(');
+        } else {
+            return JSON.parse(xhr.responseText);
+        }
     }
 
+    var messages = getMessages(),
+        li_items = new DocumentFragment();
+
+    messages.forEach((message) => {
+        var li = document.createElement('li');
+        li.innerText = message.text;
+        li_items.appendChild(li);
+    });
+
+    document.getElementById('messages').appendChild(li_items);
 
 });
