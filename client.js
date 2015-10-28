ee4030f4ccd6d533fdd1a44859221d26304c6ec2
Arthur Udalov <artur.udalov@gmail.com>
2015-10-29 00:14:37 +0300

    Move model and view into global scope

--- client.js
+++ client.js
@@ -1,27 +1,27 @@
+function getMessages() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/messages', false);
+    xhr.send();
+    if (xhr.status != 200) {
+        alert('Не вышло :(');
+    } else {
+        return JSON.parse(xhr.responseText);
+    }
+}
+
+function showMessages(ul, messages) {
+    var li_items = new DocumentFragment();
+
+    messages.forEach((message) => {
+        var li = document.createElement('li');
+        li.innerText = message.text;
+        li_items.appendChild(li);
+    });
+
+    ul.appendChild(li_items);
+}
+
 document.addEventListener('DOMContentLoaded',() => {
-
-    function getMessages() {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', '/messages', false);
-        xhr.send();
-        if (xhr.status != 200) {
-            alert('Не вышло :(');
-        } else {
-            return JSON.parse(xhr.responseText);
-        }
-    }
-
-    function showMessages(ul, messages) {
-        var li_items = new DocumentFragment();
-
-        messages.forEach((message) => {
-            var li = document.createElement('li');
-            li.innerText = message.text;
-            li_items.appendChild(li);
-        });
-
-        ul.appendChild(li_items);
-    }
 
     showMessages(
         document.getElementById('messages'),
